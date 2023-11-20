c60c1d90b70b4208e2b3b5eb118ee7eb5600b9e1
ANGEL SNOWLITA S <124783379+Snowlita2004@users.noreply.github.com>
2023-11-20 22:28:31 +0530

    Delete day 7 directory

--- day 7/about.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function About() {
-  return (
-    <div>In tha about the page</div>
-  )
-}
--- day 7/home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Home() {
-  return (
-    <div>In the home page</div>
-  )
-}
